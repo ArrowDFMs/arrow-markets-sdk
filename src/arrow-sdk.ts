12d138f301c6317e1a87dd23ef3b9666bbcf76bf
A.Tena <tena@subharmonic.tech>
2022-09-18 13:04:33 -0400

    formatting

--- src/arrow-sdk.ts
+++ src/arrow-sdk.ts
@@ -7,7 +7,7 @@ import {
   IERC20Metadata,
   IArrowRouter,
   IArrowEvents,
-  IArrowRegistry,
+  IArrowRegistry
 } from "../abis";
 
 import { ArrowOptionChainProxy } from "../build";
@@ -17,11 +17,11 @@ import { ArrowOptionChainProxy } from "../build";
  **************************************/
 
 export interface Greeks {
-  delta: number;
-  gamma: number;
-  rho: number;
-  theta: number;
-  vega: number;
+  delta: number,
+  gamma: number,
+  rho: number,
+  theta: number,
+  vega: number,
 }
 
 export interface Option {
