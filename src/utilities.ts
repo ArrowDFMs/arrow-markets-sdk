ad0956e00ba8f5c99820cdfe48dc0162510a53dc
A.Tena <tena@subharmonic.tech>
2022-09-27 15:14:18 -0400

    Pr comments

--- src/utilities.ts
+++ src/utilities.ts
@@ -195,7 +195,7 @@ export async function getUnderlierPriceHistory(
 
     const {
         data: {
-            market_caps,
+            marketCaps,
             prices
         }
     } = await axios.get<GetUnderlierHistoricalPricesResponse>(
@@ -214,7 +214,7 @@ export async function getUnderlierPriceHistory(
 
     const priceHistory = prices.map((entry) => entry[1])
 
-    return {priceHistory, market_caps}
+    return {priceHistory, marketCaps}
 }
 
 /**
@@ -231,12 +231,12 @@ export async function getUnderlierPriceAndHistory(
     currency = Currency.USD
 ) {
     const spotPrice = await getUnderlierSpotPrice(ticker)
-    const {priceHistory, market_caps} = await getUnderlierPriceHistory(ticker, days, currency)
+    const {priceHistory, marketCaps} = await getUnderlierPriceHistory(ticker, days, currency)
 
     return {
         spotPrice,
         priceHistory,
-        market_caps
+        marketCaps
     }
 }
 
