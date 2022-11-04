f7a494692535cae74ca24f395bdb9d4a0921a29f
A.Tena <tenathedev@gmail.com>
2022-11-04 14:35:26 -0400

    import AbstractArrowShortAggregator from build files

--- build/index.ts
+++ build/index.ts
@@ -2,7 +2,6 @@ import { Version } from "../src/types"
 
 import ArrowOptionChainProxyV3 from "./v3/ArrowOptionChainProxy.json"
 import ArrowOptionChainProxyV4 from "./v4/ArrowOptionChainProxy.json"
-import AbstractArrowShortAggregatorV4 from "./v4/AbstractArrowShortAggregator.json"
 import ArrowOptionChainProxyCompetition from "./competition/ArrowOptionChainProxy.json"
 
 const ArrowOptionChainProxy: Record<Version, any> = {
@@ -11,10 +10,4 @@ const ArrowOptionChainProxy: Record<Version, any> = {
     [Version.COMPETITION]: ArrowOptionChainProxyCompetition
 }
 
-const AbstractArrowShortAggregator: Record<Version, any> = {
-    [Version.V3]: AbstractArrowShortAggregatorV4,
-    [Version.V4]: AbstractArrowShortAggregatorV4,
-    [Version.COMPETITION]: AbstractArrowShortAggregatorV4
-}
-
-export { ArrowOptionChainProxy, AbstractArrowShortAggregator }
+export { ArrowOptionChainProxy }
