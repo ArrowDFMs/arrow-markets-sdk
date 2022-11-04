bb3930082bf0932361c649910c3ec050afd1cd9b
A.Tena <tenathedev@gmail.com>
2022-11-04 14:02:25 -0400

    update example script

--- examples/place-option-order.ts
+++ examples/place-option-order.ts
@@ -2,7 +2,7 @@
 // but this does not have to be the way that you choose to inject your own private key into this example code.
 import dotenv from "dotenv"
 dotenv.config()
-import ERC20ABI from '../build/ERC20.json'
+
 // Imports
 import {
     ContractType,
@@ -10,8 +10,7 @@ import {
     OptionContract,
     OptionOrderParams,
     OrderType,
-    Ticker,
-    Version
+    Ticker
 } from "../lib/src/types"
 import arrowsdk from "../lib/src/arrow-sdk"
 import { ethers } from "ethers"
@@ -23,7 +22,7 @@ dayjs.extend(utc)
 dayjs.extend(customParseFormat)
 
 // Get wallet using private key from .env file
-const wallet = new ethers.Wallet('59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d', arrowsdk.providers.fuji)
+const wallet = new ethers.Wallet(process.env.PRIVATE_KEY!, arrowsdk.providers.fuji)
 
 async function main() {
     // Specify version
@@ -43,10 +42,10 @@ async function main() {
 
     // Option order parameters
     const option: OptionContract = {
-        ticker: Ticker.ETH,
+        ticker: Ticker.AVAX,
         expiration: readableExpiration, // The next nearest friday from today
-        strike: [1000,0], // Note that the ordering of the strikes is always [long, short] for spreads and always [long, 0] for single calls/puts
-        contractType: ContractType.PUT, // 0 for call, 1 for put, 2 for call spread, and 3 for put spread
+        strike: [87.02, 84.0], // Note that the ordering of the strikes is always [long, short] for spreads and always [long, 0] for single calls/puts
+        contractType: ContractType.PUT_SPREAD, // 0 for call, 1 for put, 2 for call spread, and 3 for put spread
     }
     
     // Get current price of underlying asset from Binance/CryptoWatch and 12 weeks of price history from CoinGecko.
@@ -55,72 +54,51 @@ async function main() {
     
     // Estimate option price by making API request.
     const optionOrderParams: OptionOrderParams = {
-        quantity: 1.0, // 2.0 contracts
+        quantity: 2.0, // 2.0 contracts
         ...option,
-        orderType: OrderType.SHORT_OPEN
+        orderType: OrderType.LONG_OPEN
     }
     const estimatedOptionPrice = await arrowsdk.estimateOptionPrice(optionOrderParams, version)
-    console.log('optionOrderParams',optionOrderParams)
-    console.log('version',version)
-    console.log('estimatedOptionPrice is', estimatedOptionPrice)
 
     // Prepare the order parameters.
     // Below, we set a threshold price for which any higher (for option buy) or lower (for option sell) price will be rejected in the option order.
     // For this example, we choose to set our thresholdPrice to be equal to the estimatedOptionPrice.
-    optionOrderParams.thresholdPrice = estimatedOptionPrice - (estimatedOptionPrice * 0.02)
+    optionOrderParams.thresholdPrice = estimatedOptionPrice
 
     // Prepare the option order parameters
     const deliverOptionParams: DeliverOptionParams = await arrowsdk.prepareDeliverOptionParams(optionOrderParams, version, wallet)
-    console.log('deliverOptionParams', deliverOptionParams)
-    // // Get computed option chain address
-    const shortAggregatorAddress = await arrowsdk.computeShortAggregatorAddress(option.ticker, version)
-    console.log('shortAggregatorAddress',shortAggregatorAddress)
     
+    // Get computed option chain address
+    const optionChainAddress = await arrowsdk.computeOptionChainAddress(option.ticker, option.expiration, version)
+
     // Approval circuit if the order is a "buy" order
-    if (deliverOptionParams.orderType === OrderType.LONG_OPEN || deliverOptionParams.orderType === OrderType.SHORT_OPEN) {
+    if (deliverOptionParams.orderType === OrderType.LONG_OPEN) {
         // Get user's balance of stablecoin
         const userBalance = await stablecoin.balanceOf(wallet.address)
-        console.log('stablecoin addrs', stablecoin.address)
-       const ausd = new ethers.Contract(
-        await stablecoin.address,
-        ERC20ABI.abi,
-        wallet
-      );
-    //  await (await ausd.allocateTo(wallet.address, ethers.utils.parseUnits('1000000', await stablecoin.decimals()))).wait(numBlockConfirmations)
-     
-        console.log('ausd', ausd)
-        console.log('userBalance', userBalance)
-        console.log('stablecoin', stablecoin.address)
-        console.log('to approve', deliverOptionParams.amountToApprove)
+
         // If user's balance is less than the amount required for the approval, throw an error
         if (userBalance.lt(deliverOptionParams.amountToApprove)) {
             throw new Error('You do not have enough stablecoin to pay for your indicated threshold price.')
         }
 
         // Get the amount that the option chain proxy is currently approved to spend
-        let approvedAmount = await stablecoin.allowance(wallet.address, shortAggregatorAddress)
-        console.log('approvedAmount', approvedAmount)
-        console.log('Need to approve', (deliverOptionParams.amountToApprove))
-        console.log('needs approval ?', approvedAmount.lt(deliverOptionParams.amountToApprove))
-
+        let approvedAmount = await stablecoin.allowance(wallet.address, optionChainAddress)
         // If the approved amount is less than the amount required to be approved, ask user to approve the proper amount
         if (approvedAmount.lt(deliverOptionParams.amountToApprove)) {
-            console.log('In Here')
             // Wait for the approval to be confirmed on-chain
-            await (await stablecoin.approve(shortAggregatorAddress, deliverOptionParams.amountToApprove)).wait(numBlockConfirmations)
-            
+            await (await stablecoin.approve(optionChainAddress, deliverOptionParams.amountToApprove)).wait(numBlockConfirmations)
+
             // Get the amount that the option chain proxy is approved to spend now
-            approvedAmount = await stablecoin.allowance(wallet.address, shortAggregatorAddress)
-            console.log('new approvedAmount', approvedAmount)
+            approvedAmount = await stablecoin.allowance(wallet.address, optionChainAddress)
             // If the newly approved amount is still less than the amount required to be approved, throw and error
-            if (approvedAmount.lt(ethers.utils.parseUnits((deliverOptionParams.amountToApprove).toString()))) {
+            if (approvedAmount.lt(deliverOptionParams.amountToApprove)) {
                 throw new Error('Approval to option chain failed.')
             }
         }
     }
-    console.log('about to submit');
+
     // Submit order to API and get response
-    const { tx_hash, execution_price } = await arrowsdk.submitOptionOrder(deliverOptionParams, Version.V4)
+    const { tx_hash, execution_price } = await arrowsdk.submitOptionOrder(deliverOptionParams, version)
 
     console.log("Transaction hash:", tx_hash) // Transaction has of option order on Arrow
     console.log("Execution price:", execution_price) // The price the user ended up paying for each option in their order
