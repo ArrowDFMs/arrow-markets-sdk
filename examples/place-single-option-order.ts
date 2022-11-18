7008052f1407b83eed94747c657762b4e02cd470
A.Tena <tenathedev@gmail.com>
2022-11-18 06:31:28 -0500

    Update example scripts

--- examples/place-single-option-order.ts
+++ examples/place-single-option-order.ts
@@ -66,39 +66,39 @@ async function main() {
     optionOrderParams.thresholdPrice = estimatedOptionPrice
 
     // Prepare the option order parameters
-    const deliverOptionParams: DeliverOptionParams = await arrowsdk.prepareDeliverOptionParams(optionOrderParams, version, wallet)
+    const deliverOptionParams: DeliverOptionParams[] = await arrowsdk.prepareDeliverOptionParams([optionOrderParams], version, wallet)
     
     // Get computed option chain address
     const optionChainAddress = await arrowsdk.computeOptionChainAddress(option.ticker, option.expiration, version)
 
     // Approval circuit if the order is a "buy" order
-    if (deliverOptionParams.orderType === OrderType.LONG_OPEN) {
+    if (deliverOptionParams[0].orderType === OrderType.LONG_OPEN) {
         // Get user's balance of stablecoin
         const userBalance = await stablecoin.balanceOf(wallet.address)
 
         // If user's balance is less than the amount required for the approval, throw an error
-        if (userBalance.lt(deliverOptionParams.amountToApprove)) {
+        if (userBalance.lt(deliverOptionParams[0].amountToApprove)) {
             throw new Error('You do not have enough stablecoin to pay for your indicated threshold price.')
         }
 
         // Get the amount that the option chain proxy is currently approved to spend
         let approvedAmount = await stablecoin.allowance(wallet.address, optionChainAddress)
         // If the approved amount is less than the amount required to be approved, ask user to approve the proper amount
-        if (approvedAmount.lt(deliverOptionParams.amountToApprove)) {
+        if (approvedAmount.lt(deliverOptionParams[0].amountToApprove)) {
             // Wait for the approval to be confirmed on-chain
-            await (await stablecoin.approve(optionChainAddress, deliverOptionParams.amountToApprove)).wait(numBlockConfirmations)
+            await (await stablecoin.approve(optionChainAddress, deliverOptionParams[0].amountToApprove)).wait(numBlockConfirmations)
 
             // Get the amount that the option chain proxy is approved to spend now
             approvedAmount = await stablecoin.allowance(wallet.address, optionChainAddress)
             // If the newly approved amount is still less than the amount required to be approved, throw and error
-            if (approvedAmount.lt(deliverOptionParams.amountToApprove)) {
+            if (approvedAmount.lt(deliverOptionParams[0].amountToApprove)) {
                 throw new Error('Approval to option chain failed.')
             }
         }
     }
 
     // Submit order to API and get response
-    const { tx_hash, execution_price } = await arrowsdk.submitOptionOrder(deliverOptionParams, version)
+    const { tx_hash, execution_price } = await arrowsdk.submitLongOptionOrder(deliverOptionParams, version)
 
     console.log("Transaction hash:", tx_hash) // Transaction has of option order on Arrow
     console.log("Execution price:", execution_price) // The price the user ended up paying for each option in their order
